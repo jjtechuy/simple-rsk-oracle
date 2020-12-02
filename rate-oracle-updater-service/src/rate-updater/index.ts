ce06e8b9afcc8c16629b0d73de4fc6d4db309557
Nazar Duchak <nazar@iovlabs.org>
2020-12-02 20:46:29 +0200

    feat: make oracle contract call when need to update rate

--- rate-oracle-updater-service/src/rate-updater/index.ts
+++ rate-oracle-updater-service/src/rate-updater/index.ts
@@ -1,7 +1,9 @@
 import config from 'config'
+import { Eth } from 'web3-eth'
 
 import { Application, RateUpdaterService } from '../definitions'
 import { loggingFactory } from '../logger'
+import { RateOracleContract } from '../oracle-contract'
 import { RateProviderManager } from '../rate-provider'
 import { waitForReadyApp } from '../utils'
 import { OracleUpdateEvent, RateUpdateTrigger } from './update-trigger'
@@ -12,7 +14,8 @@ const upload: RateUpdaterService = {
   async initialize (app: Application): Promise<{ stop: () => Promise<void> }> {
     await waitForReadyApp(app)
     const rateProviderManager = app.get('rateProvider') as RateProviderManager
-    // const oracleContract = app.get('oracleContract')
+    const eth = app.get('eth') as Eth
+    const oracleContract = new RateOracleContract(eth, config.get<string>('oracle.account'))
 
     if (!config.has('rateUpdateThreshold')) {
       throw new Error('Rate update threshold not configured!')
@@ -23,24 +26,24 @@ const upload: RateUpdaterService = {
     }
 
     const rateUpdateTrigger = new RateUpdateTrigger(
-        rateProviderManager,
-        config.get<number>('rateUpdateThreshold'),
-        config.get<number>('rateApi.ratePollInterval'),
-        config.get<number>('rateUpdateInterval')
+      rateProviderManager,
+      config.get<number>('rateUpdateThreshold'),
+      config.get<number>('rateApi.ratePollInterval'),
+      config.get<number>('rateUpdateInterval')
     )
 
     rateUpdateTrigger.on(
-        OracleUpdateEvent,
-        async (rate: number) => {
-          logger.info(`Need to update Oracle with rate ${rate}`)
-          // TODO Implement oracle update rate transaction
-          // await oracleContract.updateRate(rate)
-        }
+      OracleUpdateEvent,
+      async (rate: number) => {
+        logger.info(`Need to update Oracle with rate ${rate}`)
+        logger.debug(await oracleContract.updateRate(rate))
+      }
     )
 
     await rateUpdateTrigger.run('BTC', 'USD')
 
     return {
+      // eslint-disable-next-line require-await
       stop: async () => {
         rateUpdateTrigger.stop()
       }
